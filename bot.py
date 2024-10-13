4590def23750651c895fb6341926733087520032
KeremAbdurrahim <keremyilmazer6@gmail.com>
2024-10-14 00:39:45 +0300

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,24 @@
+import discord
+from  Pythonpro import sifreolusturucu
+# ayricaliklar (intents) değişkeni botun ayrıcalıklarını depolayacak
+intents = discord.Intents.default()
+# Mesajları okuma ayrıcalığını etkinleştirelim
+intents.message_content = True
+# client (istemci) değişkeniyle bir bot oluşturalım ve ayrıcalıkları ona aktaralım
+client = discord.Client(intents=intents)
+@client.event
+async def on_ready():
+    print(f'{client.user} olarak giriş yaptık.')
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+    if message.content.startswith('hello'):
+        await message.channel.send("Selam!")
+    elif message.content.startswith('bye'):
+        await message.channel.send("\U0001f642")
+    elif message.content.startswith('sifre'):
+        await message.channel.send(sifreolusturucu())
+    else:
+        await message.channel.send(message.content)
+client.run("token")
